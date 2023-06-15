1ef799a71e417ff50467b51d46609d1e5c0fcd68
Nikolai <nikolai.ivanov3110@gmail.com>
2023-06-15 18:08:50 +0400

    feat: implement src/hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import * as fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+
+  const file = './fileToCalculateHashFor.txt';
+  const dir = '/files';
+  const calcHashPath = fileURLToPath(import.meta.url);
+  const directory = path.dirname(calcHashPath);
+  const filePath = path.join(directory, dir, file);
+
+  try {
+    const fileData = await fs.promises.readFile(filePath);
+    const hash = crypto.createHash('sha256').update(fileData).digest('hex');
+    console.log(hash);
+  } catch (error) {
+    console.error('Failed to calculate hash');
+  }
 };
 
 await calculateHash();
