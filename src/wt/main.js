03064da57eba263947d541f40992a2f7c3f2ae3f
Nikolai <nikolai.ivanov3110@gmail.com>
2023-06-15 19:46:02 +0400

    feat: implement src/wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const worker = 'worker.js';
+    const mainFilePath = fileURLToPath(import.meta.url);
+    const dir = path.dirname(mainFilePath);
+    const workerPath = path.join(dir, worker);
+
+
+  const numWorkers = cpus().length;
+  const workerPromises = [];
+
+  for (let i = 0; i < numWorkers; i++) {
+    const worker = new Worker(workerPath, { workerData: { number: i + 10 } });
+    const promise = new Promise((resolve, reject) => {
+      worker.on('message', (message) => {
+        resolve(message);
+      });
+
+      worker.on('error', (error) => {
+        reject(error);
+      });
+    });
+
+    workerPromises.push(promise);
+  }
+
+  const results = await Promise.all(workerPromises);
+  console.log(results);
 };
 
 await performCalculations();
