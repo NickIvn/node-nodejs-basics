529d0bd0c0641fef11aa6e24148e40723a704d02
Nikolai <nikolai.ivanov3110@gmail.com>
2023-06-15 19:28:05 +0400

    feat: implement src/cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,41 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const file = 'script.js';
+    const folder = 'files';
+    const cpFilepath = fileURLToPath(import.meta.url);
+    const dir = path.dirname(cpFilepath);
+    const filePath = path.join(dir, folder, file);
+
+  const childProcess = spawn('node', [filePath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit'],
+  });
+
+  childProcess.stdout.on('data', (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  process.stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  process.stdin.on('end', () => {
+    childProcess.stdin.end();
+  });
+
+  return new Promise((resolve, reject) => {
+    childProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Child process exited with code ${code}`));
+      }
+    });
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['first', 'second', 'third']);
+
