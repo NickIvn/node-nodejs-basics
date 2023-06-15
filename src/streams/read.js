e78d5f9af43f7af9a1d2cdea3f6b90ced1741404
Nikolai <nikolai.ivanov3110@gmail.com>
2023-06-15 18:51:29 +0400

    feat: implement src/streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const file = 'fileToRead.txt';
+    const dir = 'files'
+
+    const readFilePath = fileURLToPath(import.meta.url);
+    const directory = path.dirname(readFilePath);
+    const filePath = path.join(directory, dir, file);
+  
+  try {
+    const readableStream = fs.createReadStream(filePath);
+    
+    readableStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+    });
+    
+    readableStream.on('end', () => {
+      console.log('\nReading completed');
+    });
+  } catch (error) {
+    console.error('Failed to read file');
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
-};
+    const file = 'fileToWrite.txt';
+    const dir = 'files'
+
+    const writeFilePath = fileURLToPath(import.meta.url);
+    const directory = path.dirname(writeFilePath);
+    const filePath = path.join(directory, dir, file);
+    
+    try {
+        const writableStream = fs.createWriteStream(filePath);
+    
+        process.stdin.on('data', (data) => {
+          writableStream.write(data);
+        });
+    
+        process.stdin.on('end', () => {
+          writableStream.end();
+          console.log('Writing completed');
+        });
+      } catch (error) {
+        console.error('Failed to write file');
+      }
+    };
+    
 
 await write();
