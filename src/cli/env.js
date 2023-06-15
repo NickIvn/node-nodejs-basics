7679b4ce0b50d26c3862be14ecccd6bdc7587ada
Nikolai <nikolai.ivanov3110@gmail.com>
2023-06-15 18:08:36 +0400

    feat: implement src/cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+
+  for (const key in process.env) {
+    if (key.startsWith(prefix)) {
+      const value = process.env[key];
+      const name = key.slice(prefix.length);
+      console.log(`RSS_${name}=${value}`);
+    }
+  }
 };
 
 parseEnv();
